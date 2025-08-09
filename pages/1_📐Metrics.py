e210d0d2ff7695597cb4347319b20744a4e82f1c
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-08-09 15:31:16 +0330

    Update 1_📐Metrics.py

--- pages/1_📐Metrics.py
+++ pages/1_📐Metrics.py
@@ -7,35 +7,15 @@ import plotly.graph_objects as go
 
 # --- Page Config ------------------------------------------------------------------------------------------------------
 st.set_page_config(
-    page_title="Squid Bridge Metrics",
-    page_icon="https://img.cryptorank.io/coins/squid1675241862798.png",
+    page_title="Axelar: An Overview",
+    page_icon="https://img.cryptorank.io/coins/axelar1663924228506.png",
     layout="wide"
 )
 
-# --- Title with Logo -----------------------------------------------------------------------------------------------------
-st.markdown(
-    """
-    <div style="display: flex; align-items: center; gap: 15px;">
-        <img src="https://img.cryptorank.io/coins/squid1675241862798.png" alt="Squid Logo" style="width:60px; height:60px;">
-        <h1 style="margin: 0;">Squid Bridge Metrics</h1>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# --- Title  -----------------------------------------------------------------------------------------------------
+st.title("📐Metrics")
 
-# --- Builder Info ---------------------------------------------------------------------------------------------------------
-st.markdown(
-    """
-    <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
-        <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" style="width:25px; height:25px; border-radius: 50%;">
-            <span>Built by: <a href="https://x.com/0xeman_raz" target="_blank">Eman Raz</a></span>
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
+# --- attention ---------------------------------------------------------------------------------------------------------
 st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
 st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
